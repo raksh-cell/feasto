ae1de0764cefb40f5cb907d9b8a9c3b2aa0fa939
Rakshith Shetty <RakshithShetty@example.com>
2025-06-26 18:22:42 +0530

    added both implicit and explicit animations to make the app feel smoother. Also added nice transitions when moving between screens to improve the overall user experience.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,7 +6,7 @@ import 'package:feasto/screens/meals.dart';
 import 'package:flutter/material.dart';
 import 'package:feasto/widgets/categories_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
 
@@ -15,8 +15,39 @@ class CategoriesScreen extends StatelessWidget {
 
   final List<Meal>availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+
+    super.initState();
+    _animationController=AnimationController(
+      vsync:this,
+      duration: const Duration(milliseconds: 300,),
+      lowerBound:0,
+      upperBound: 1,
+      );  
+      _animationController.forward();
+  }
+
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+
+
+
+
+
   void _selectCategory(BuildContext context,Category category ){
-    final filteredMeals=availableMeals
+    final filteredMeals=widget.availableMeals
     .where((meal)=>
     meal.categories
     .contains(category.id)).
@@ -35,7 +66,9 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  GridView(
+    return AnimatedBuilder(
+      animation: _animationController, 
+      child: GridView(
         padding: const EdgeInsets.all(24), // Optional: for spacing around the grid
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -53,7 +86,19 @@ class CategoriesScreen extends StatelessWidget {
             },
             ),
         ],
-      );
+      ),
+      builder: (context,child)=>SlideTransition(
+        position: Tween(
+            begin: const Offset(0, 0.3),
+            end: const Offset(0, 0),
+          ).animate(CurvedAnimation(
+            parent: _animationController, 
+            curve: Curves.easeInOut
+            ),
+          ),
 
+          child:child,
+       ),
+        );
   }
 }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -40,12 +40,16 @@ class MealItem extends StatelessWidget {
         },
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            //animate //across diff screen
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
